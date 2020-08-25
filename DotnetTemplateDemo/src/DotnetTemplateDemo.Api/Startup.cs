057fc44659534359c3ae14cab488cd4fb1354495
onelioubov <onelioubov@gmail.com>
2020-08-25 18:43:49 -0400

    wired up AppInsights in the API project

--- DotnetTemplateDemo/src/DotnetTemplateDemo.Api/Startup.cs
+++ DotnetTemplateDemo/src/DotnetTemplateDemo.Api/Startup.cs
@@ -19,6 +19,7 @@ namespace DotnetTemplateDemo.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
             services.AddControllers();
             services.AddHealthChecks();
 
