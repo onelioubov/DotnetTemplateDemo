e25852355f4d8fbb4441822347b6f759a102bfcb
onelioubov <onelioubov@gmail.com>
2020-09-23 21:37:26 -0400

    Added a repo initialization script.

---
+++ DotnetTemplateDemo/initializeGitRepo.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Creating new local repository..."
+if ! git status > /dev/null 2>&1
+then
+  git init
+else
+  echo "Local repository already exists."
+fi
+
+echo -e "\nCreating [main] branch..."
+if git checkout -b main
+then
+  echo -e "\nMaking initial commit..."
+  git add .
+  git commit -m "initial commit"
+  git branch -M main
+else
+  echo "Please see the following log for commit details."
+  git log
+fi
+
+echo -e "\nSetting up remote repo..."
+if gh help > /dev/null 2>&1
+then
+  tryCreateRemote=$(gh repo create DotnetTemplateDemo --public --confirm 2>/dev/null)
+  if [ $? -eq 0 ]
+  then
+    echo "Created new repository $tryCreateRemote and added as remote."
+    echo "If you're deploying to cloud resources, make sure to set those resources up before you push [main] to [origin]."
+  else
+    echo "Remote repository already exists."
+  fi
+else
+  echo "Please install GitHub CLI. For more info, visit https://cli.github.com/."
+fi
