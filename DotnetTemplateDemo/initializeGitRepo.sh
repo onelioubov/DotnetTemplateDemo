2a953a853f629af3bf7e43b1321655c763c55341
onelioubov <onelioubov@gmail.com>
2020-09-23 22:14:22 -0400

    Added a comment to not run the repo initialization script until variable issue has been resolved.

--- DotnetTemplateDemo/initializeGitRepo.sh
+++ DotnetTemplateDemo/initializeGitRepo.sh
@@ -23,6 +23,16 @@ fi
 echo -e "\nSetting up remote repo..."
 if gh help > /dev/null 2>&1
 then
+  # The fact that the name of the repo that this is creating is the same as the Demo repo name can potentially be 
+  # problematic if the variable replacement doesn't work for whatever reason. I don't think it's going to override it,
+  # but it's still bad design. Will need to do one of the following things (this is mostly so I remember, in case I
+  # forgot about this):
+  #
+  # a) Rename DotnetTemplateDemo repo to something different from the service name variables it's replacing.
+  # b) Rename all the occurrences of DotnetTemplateDemo inside the repo (but not the repo itself).
+  # c) Create a one-off auto-implemented template parameter to replace a different variable, which will be used here.
+  # 
+  # Until one of the above is implemented, DO NOT run this script (even though it totally works).
   tryCreateRemote=$(gh repo create DotnetTemplateDemo --public --confirm 2>/dev/null)
   if [ $? -eq 0 ]
   then
